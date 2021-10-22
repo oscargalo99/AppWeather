a40c5f12a65c2af0df546da54a488fffdbbe4d18
Oscar Gallego <oscargalo99@gmail.com>
2021-10-22 19:19:08 +0100

    Proyecto Weather V2

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,33 @@
+var onSuccess = function(position) {
+
+  var apiKey = "de75094b22ef2f55c90aaa6e442bb050";
+
+  $.get( "https:api.openweathermap.org/data/2.5/weather?lat="+ position.coords.latitude +"&lon="+ position.coords.longitude +"&appid="+apiKey,
+  function(tiempo) {
+
+    var nombre = tiempo.name
+    var descripcion = tiempo.weather[0].main;
+    var icono = tiempo  .weather[0].icon;
+    var temperaturaK = tiempo.main.temp;
+    var temperatura = temperaturaK -  273.15;
+
+    $(".localizacion").append(
+      '<div class="card text-center shadow p-3 mb-5 bg-white rounded " style="width: 18rem;"> <div class="card-body"><h3 class="card-title">'+nombre+'<h3 class="card-title">'+descripcion+'</h3><p class="card-text"><h5>'+temperatura.toFixed()+'ºC</h5><br><img src="http://openweathermap.org/img/wn/' + icono + '@2x.png" class="img-fluid"></p></div></div></div>'
+    )}
+  )
+};
+
+function onError(error) {
+  alert("Ha habido un problema con la localización actual");
+}
+
 $(function(){
 
     $("#informacion").show();
     $(".formulario").hide();
     $(".card").hide();
     $(".ciudades").hide();
+    $(".localizacion").hide();
 
     $("#btnHome").click(function(event){
         event.preventDefault();
@@ -11,13 +35,23 @@ $(function(){
         $(".formulario").hide();
         $(".card").hide();
         $(".ciudades").hide();
+        $(".localizacion").hide();
     });
 
     $("#btnSearch").click(function(event){
         event.preventDefault();
         $("#informacion").hide();
         $(".formulario").show();
+        $(".localizacion").hide();
     });
+
+    $("#btnLocation").click(function(event){
+      event.preventDefault();
+      $("#informacion").hide();
+      $(".formulario").hide();
+      $(".localizacion").show();
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  });
 
     const form = document.querySelector(".container form");
 
@@ -57,3 +91,4 @@ $(function(){
 
     });
 })
+
